ed3594b1a0138dd283a315b279a04138fc0f23ad
TopsterTX <bbs.100@yandex.ru>
2023-08-29 18:26:01 +0300

    master [init] init project

---
+++ README.md
@@ -0,0 +1,8 @@
+Todo app
+
+stack:
+
+1. React
+2. MobX
+
+pattern: MVVM
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Layout } from "common/index";
+import "./App.css";
+
+const App = () => {
+  <Layout></Layout>;
+};
+
+export default App;
